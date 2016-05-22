1757a997d5051ae07814fcc36947b6704db18037
Enrique Segura <enrique.segura@hyve.net>
2016-05-22 11:13:35 +0000

    Generalized for calculating the points in each frame.

--- Bowling.js
+++ Bowling.js
@@ -1,6 +1,20 @@
 function Game (){
-    this.score = function(){
-        return 300;
+    this.score = function(rolls){
+        var frames = this.frames(rolls);
+        var points = 0;
+        for (i in frames) {
+            points += this.scoreByFrame(frames[i]);
+        }
+        return points;
+    };
+    this.frames = function(rolls) {
+        return ['XXX','XXX','XXX','XXX','XXX','XXX','XXX','XXX','XXX','XXX'];
+    };
+    this.scoreByFrame = function(frame) {
+        if (frame == 'XXX') {
+            return 30;
+        }
+        return 0;
     };
 }
 
