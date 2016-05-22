d39a3ac8c68db9d5cbf2ef0b96b5cb03df609349
Enrique Segura <enrique.segura@hyve.net>
2016-05-22 10:37:20 +0000

    Define function to calculate score in base of the rolls.

--- BowlingTest.js
+++ BowlingTest.js
@@ -2,6 +2,14 @@
  * http://butunclebob.com/ArticleS.UncleBob.TheBowlingGameKata
  */
 
+/*
+  TODO
+  * Scores to test:
+    "XXXXXXXXXXXX" (12 rolls: 12 strikes) = 10+10+10 + 10+10+10 + 10+10+10 + 10+10+10 + 10+10+10 + 10+10+10 + 10+10+10 + 10+10+10 + 10+10+10 + 10+10+10 = 300
+    "9-9-9-9-9-9-9-9-9-9-" (20 rolls: 10 pairs of 9 and miss) = 9 + 9 + 9 + 9 + 9 + 9 + 9 + 9 + 9 + 9 = 90
+     "5/5/5/5/5/5/5/5/5/5/5" (21 rolls: 10 pairs of 5 and spare, with a final 5) = 10+5 + 10+5 + 10+5 + 10+5 + 10+5 + 10+5 + 10+5 + 10+5 + 10+5 + 10+5 = 150
+*/
+
 var Game = require('./Bowling.js');
 
 describe('The Bowling game kata', function(){
@@ -10,7 +18,8 @@ describe('The Bowling game kata', function(){
     g = new Game();
   });
 
-  it('First test, the game is created.' , function(){
-    expect(g.score()).toBe(0);
+  it('First test, the game is created and it has and score function that accept the rolls.' , function(){
+    rolls = '';
+    expect(g.score(rolls)).toBe(0);
   });
 });
