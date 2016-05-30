217f64bc5805a92de8e9870035d82e37ec649344
enriquesegura <segura.gcc@gmail.com>
2016-05-30 08:16:57 +0200

    Update BowlingTest.js

--- BowlingTest.js
+++ BowlingTest.js
@@ -2,18 +2,6 @@
  * http://butunclebob.com/ArticleS.UncleBob.TheBowlingGameKata
  */
 
-/*
-  TODO
-  * Scores to test:
-    "XXXXXXXXXXXX" (12 rolls: 12 strikes) = 10+10+10 + 10+10+10 + 10+10+10 + 10+10+10 + 10+10+10 + 10+10+10 + 10+10+10 + 10+10+10 + 10+10+10 + 10+10+10 = 300
-    "9-9-9-9-9-9-9-9-9-9-" (20 rolls: 10 pairs of 9 and miss) = 9 + 9 + 9 + 9 + 9 + 9 + 9 + 9 + 9 + 9 = 90
-     "5/5/5/5/5/5/5/5/5/5/5" (21 rolls: 10 pairs of 5 and spare, with a final 5) = 10+5 + 10+5 + 10+5 + 10+5 + 10+5 + 10+5 + 10+5 + 10+5 + 10+5 + 10+5 = 150
-  * Examples of frame points:
-    Strike: "X--" (10 points), "XXX" (30 points), "X-/" (20 points), "X23" (15 points).
-    Spare: "-/X" (20 points), "3/-" (10 points), "4/1" (11 points), "9/X" (20 points).
-    < 10 pins: "36" (9 points), "--" (0 points), "3-" (3 points), "-4" (4 points), "17" (8 points)
-*/
-
 var Game = require('./Bowling.js');
 
 describe('The Bowling game kata', function(){
